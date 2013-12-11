3a0c8671e34b0951187f1b51d67cb1b4bc5c5d77
Eduardo Rabelo <oieduardorabelo@gmail.com>
2013-12-11 11:19:11 -0200

    new git alias for workflow

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -15,6 +15,8 @@ alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
+alias ga='git add'
+alias gaa='git add -A'
+alias gcm='git commit -m'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
-           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+alias grm="git status | grep deleted | awk '{print \$2}' | xargs git rm"
